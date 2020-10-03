095a1940a6f09c45cdf10a221524aad42f35881b
Yulia Aleinik <aleinikya@mail.ru>
2020-10-03 18:33:04 +0300

    fix: fix array's problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined) {
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  let minNumber = array[0];
+  for (i = 0; i < array.length; i++) {
+    if (array[i] < minNumber) {
+      minNumber = array[i];
+    }
+  }
+  return minNumber;
 }
 
-exports.max = function max (array) {
-  return 0;
+
+
+exports.max = function max(array) {
+  if (array === undefined) {
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  let maxNumber = array[0];
+  for (i = 0; i < array.length; i++) {
+    if (array[i] > maxNumber) {
+      maxNumber = array[i];
+    }
+  }
+
+  return maxNumber;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+
+exports.avg = function avg(array) {
+  if (array === undefined) {
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum / array.length
 }
+
+
